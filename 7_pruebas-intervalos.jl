5ad2543216f5e0ceb85d58ea15b3c2276c451e60
Mario Martínez <mmartinez.21@hotmail.com>
2015-04-14 01:06:15 -0500

    Se agregaron pruebas para las nuevas funciones
    Correspondiente al notebook_8

--- 7_pruebas-intervalos.jl
+++ 7_pruebas-intervalos.jl
@@ -6,6 +6,8 @@ facts ("Pruebas") do
   M=Interval(1.0, 5.0)
   N=Interval(3.0,7.0)
   P=Interval(-1.0, 1.0)
+  y=Interval(0.0, 1.25)
+  z=sin(y)/cos(y)
 
   @fact M+N=>Interval(4.0, 12.0)
   @fact N-M=>Interval(2.0, 2.0)
@@ -16,6 +18,12 @@ facts ("Pruebas") do
   @fact contains(Interval(.1,0.1),0.1)=>true
   @fact P^2=>Interval(0.0, 1.0)
   @fact P^3=>Interval(-1.0, 1.0)
+  @fact contains(sin(Interval(-1.5707,1.5770)),0.0)=>true
+  @fact contains(cos(Interval(0.0,3.1416)),0.0)=>true
+  @fact z.right==tan(y).right=>true
+  @fact exp(M) => Interval(exp(M.left),exp(M.right))
+  @fact exp(log(y))==y => true
+
 
 end
 
